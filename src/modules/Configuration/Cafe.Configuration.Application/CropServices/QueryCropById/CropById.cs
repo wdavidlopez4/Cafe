c6125739b783c4856d6b36ece787be8d61725fc4
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-01 21:26:55 -0500

    creacion de la logica para obtener por id un cultivo

---
+++ src/modules/Configuration/Cafe.Configuration.Application/CropServices/QueryCropById/CropById.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Configuration.Application.CropServices.QueryCropById
+{
+    public class CropById : IRequest<CropByIdDTO>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ src/modules/Configuration/Cafe.Configuration.Application/CropServices/QueryCropById/CropByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using Cafe.Configuration.Domain.Entities;
+using Cafe.Configuration.Domain.Ports;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cafe.Configuration.Application.CropServices.QueryCropById
+{
+    public class CropByIdHandler : IRequestHandler<CropById, CropByIdDTO>
+    {
+        private readonly IRepository repository;
+
+        private readonly IAutoMapping autoMapping;
+
+        public CropByIdHandler(IRepository repository, IAutoMapping autoMapping)
+        {
+            this.autoMapping = autoMapping;
+            this.repository = repository;
+        }
+
+        public async Task<CropByIdDTO> Handle(CropById request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+                throw new ArgumentNullException("la peticion para recuperar el cultivo es nula");
+
+            return autoMapping.Map<Crop, CropByIdDTO>(await this.repository.Get<Crop>(x => x.Id == request.Id, cancellationToken));
+        }
+    }
+}
