f96154bf6c8e5accdc47d6dbe86d5acc53765265
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-10 01:13:48 -0500

    creacion de la migracion para la entidad arduino

---
+++ src/modules/Configuration/Cafe.Configuration.Infrastructure/Migrations/20210410061238_CoffeeMigration2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafe.Configuration.Infrastructure.Migrations
+{
+    public partial class CoffeeMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arduino",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ConfigurationCropId = table.Column<string>(nullable: true),
+                    Occupied = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arduino", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Arduino_ConfigurationCrops_ConfigurationCropId",
+                        column: x => x.ConfigurationCropId,
+                        principalTable: "ConfigurationCrops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Arduino_ConfigurationCropId",
+                table: "Arduino",
+                column: "ConfigurationCropId",
+                unique: true,
+                filter: "[ConfigurationCropId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Arduino");
+        }
+    }
+}
