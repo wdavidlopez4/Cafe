06efe16754af7ba90297de34a8ca899d046bc0d3
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-10 21:13:19 -0500

    creacion de la api configuracion: esta api no esta completa somplemente es una guia

---
+++ src/backent/Cafe.Web.Api/Configuration/ConfigurationController.cs
@@ -0,0 +1,34 @@
+﻿using Cafe.Web.Api.Filters;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafe.Web.Api.Configuration
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [TypeFilter(typeof(ExceptionManagerConfigurationFilter))]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ConfigurationController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("Configuration")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> ObtenerConfiguracion(string idCultivo)
+        {
+            return Ok(await Task.FromResult(""));
+        }
+    }
+}
