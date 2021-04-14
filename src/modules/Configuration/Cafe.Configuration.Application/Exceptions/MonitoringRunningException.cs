261ab58b3ba67bbf94fbd7012702b561c77c4611
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-14 01:24:05 -0500

    Creacion del CU para comenzar a monitoriar manualmente

---
+++ src/modules/Configuration/Cafe.Configuration.Application/Exceptions/MonitoringRunningException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Configuration.Application.Exceptions
+{
+    public class MonitoringRunningException : Exception
+    {
+        public MonitoringRunningException(string message):base(message)
+        {
+
+        }
+    }
+}
---
+++ src/modules/Configuration/Cafe.Configuration.Application/MonitoringServices/CommandMonitoringManualBegin/MonitoringManualBeginDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Configuration.Application.MonitoringServices.CommandMonitoringManualBegin
+{
+    public class MonitoringManualBeginDTO
+    {
+        public string Id { get; set; }
+
+        public CropDTO Crop { get; set; }
+
+        public string CropId { get; set; }
+
+        public bool ActivateManually { get; set; }
+
+        public bool Activated { get; set; }
+
+        public class CropDTO
+        {
+            public string Id { get; set; }
+
+            public string Name { get; private set; }
+
+            public int DayFormation { get; private set; }
+
+            public string CoffeeGrowerId { get; private set; }
+        }
+    }
+}
