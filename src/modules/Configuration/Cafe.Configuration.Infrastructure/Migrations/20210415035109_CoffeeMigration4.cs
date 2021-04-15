5420fc7ee52a50f1ff59206b0e133b0116d34d57
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-14 22:54:08 -0500

    creacion del caso de uso para activar el monitoreo con una imagen. correccion a la migracion. migracion 4: entidad ImageMonitoring, atributo: activated

---
+++ src/modules/Configuration/Cafe.Configuration.Infrastructure/Migrations/20210415035109_CoffeeMigration4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafe.Configuration.Infrastructure.Migrations
+{
+    public partial class CoffeeMigration4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activated",
+                table: "Monitoring",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activated",
+                table: "Monitoring");
+        }
+    }
+}
