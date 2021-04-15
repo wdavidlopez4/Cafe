5657498ba11586fd58f5c26895a7a84a02ea6b92
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-15 00:20:20 -0500

    creacion de la capa de eventos de configuracion, creacion del evento del monitoreo de imagenes.

---
+++ src/modules/Configuration/Cafe.Configuration.IntegrationEvents/MonitoringEvents/MonitoringImageBeginEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Configuration.IntegrationEvents.MonitoringEvents
+{
+    public class MonitoringImageBeginEvent
+    {
+        public string Id { get; set; }
+
+        public string ActivateByImage { get; set; }
+
+        public bool Activated { get; set; }
+
+        public string CropId { get; set; }
+    }
+}
