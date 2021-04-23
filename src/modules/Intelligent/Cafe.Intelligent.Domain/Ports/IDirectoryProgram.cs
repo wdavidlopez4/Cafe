032930e9b08b9e3e8f6b0d9d573cea961e0a766e
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-22 19:54:20 -0500

    creacion de los ports; directory y repositoriyBlob, tambien creacion de la fabrica vacia y de la entidad imageData

---
+++ src/modules/Intelligent/Cafe.Intelligent.Domain/Ports/IDirectoryProgram.cs
@@ -0,0 +1,19 @@
+﻿using Cafe.Intelligent.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Intelligent.Domain.Ports
+{
+    public interface IDirectoryProgram
+    {
+        public string GetProjectDirectory();
+
+        public string CreateDirectory(string projectRootUrl, string nameDirectoryRoot);
+
+        public string CreateDirectory(string projectRootUrl, string directoryUrl, string nameDirectoryRoot);
+
+        public IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true);
+    }
+
+}
---
+++ src/modules/Intelligent/Cafe.Intelligent.Domain/Ports/IRepositoryBlob.cs
@@ -0,0 +1,34 @@
+﻿using Cafe.Intelligent.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Intelligent.Domain.Ports
+{
+    public interface IRepositoryBlob
+    {
+        /// <summary>
+        /// obtenemos las url de los blobs
+        /// </summary>
+        /// <param name="nameBlob"></param>
+        /// <returns></returns>
+        public Task<List<string>> GetNamesBlobs(string nameBlob);
+
+        /// <summary>
+        /// obtenemos los nombres de los blobs
+        /// </summary>
+        /// <param name = "nameBlob" ></ param >
+        /// < returns ></ returns >
+        public Task<List<T>> GetEntitiesBlobs<T>(string nameBlob, string label) where T : ImageData;
+
+        /// <summary>
+        /// descargar los archivos en un directorio
+        /// </summary>
+        /// <param name="DirectoryUrl"></param>
+        /// <param name="nameBlob"></param>
+        /// <returns></returns>
+        public Task DowloadBlobs(string DirectoryUrl, string nameBlob);
+    }
+
+}
