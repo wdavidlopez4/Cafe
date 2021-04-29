a31df36d9ccc439366e042a78f9beb928aeb1743
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-29 14:27:45 -0500

    comfiguracion del evento para sincronizar el arduino y arreglo del evento para el cultivo

--- src/modules/Climate/Cafe.Climate.Application/ArduinoEventHandler/ArduinoSyncUpEventHandler.cs
+++ src/modules/Climate/Cafe.Climate.Application/ArduinoEventHandler/ArduinoSyncUpEventHandler.cs
@@ -1,4 +1,8 @@
-﻿using Cafe.Configuration.IntegrationEvents.ArduinoEvents;
+﻿using Cafe.Climate.Application.Exceptions;
+using Cafe.Climate.Domain.Entities;
+using Cafe.Climate.Domain.Factories;
+using Cafe.Climate.Domain.Ports;
+using Cafe.Configuration.IntegrationEvents.ArduinoEvents;
 using JKang.EventBus;
 using System;
 using System.Collections.Generic;
@@ -9,9 +13,27 @@ namespace Cafe.Climate.Application.ArduinoEventHandler
 {
     public class ArduinoSyncUpEventHandler : IEventHandler<ArduinoSyncUpEvent>
     {
-        public Task HandleEventAsync(ArduinoSyncUpEvent @event)
+        private readonly IRepository repository;
+
+        private readonly IFactory factory;
+
+        public ArduinoSyncUpEventHandler(IRepository repository, IFactory factory)
         {
-            return Task.CompletedTask;
+            this.factory = factory;
+            this.repository = repository;
+        }
+
+        public async Task HandleEventAsync(ArduinoSyncUpEvent @event)
+        {
+            if (this.repository.Exists<Crop>(x => x.Id == @event.CropId) == false)
+                throw new EntityNullException("la operacion es invalida por que aun no se a ejecutado el evento para el cultivo");
+
+            //obtenemos el cultivo con el monitoreo
+            var crop = await this.repository.GetWithNestedObject<Crop>(x => x.Id == @event.CropId, x => x.Monitoring);
+
+            //creamos y guardamos el arduino "sincronizamos el arduino (true)"
+            var arduino = (Arduino) this.factory.CreateArduino(Guid.Parse(@event.Id), crop.Monitoring.Id, true);
+            arduino = await this.repository.Save<Arduino>(arduino);
         }
     }
 }
---
+++ src/modules/Climate/Cafe.Climate.Application/Exceptions/EntityNullException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Application.Exceptions
+{
+    public class EntityNullException : Exception
+    {
+        public EntityNullException(string message) : base(message)
+        {
+
+        }
+    }
+}
