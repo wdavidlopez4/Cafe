48ac5220f5ea23f0aafb6a3c77b7213277f44369
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-29 13:01:50 -0500

    crear puerto de repositorio

---
+++ src/modules/Climate/Cafe.Climate.Domain/Ports/IRepository.cs
@@ -0,0 +1,49 @@
+﻿using Cafe.Climate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cafe.Climate.Domain.Ports
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// almacena el objeto en la db
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public Task<T> Save<T>(T obj, CancellationToken cancellationToken) where T : EntityBase;
+
+        /// <summary>
+        /// verificar si existe una entidad
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public bool Exists<T>(Expression<Func<T, bool>> expression) where T : EntityBase;
+
+        /// <summary>
+        /// actualizar una entidad
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T> Update<T>(T obj, CancellationToken cancellationToken) where T : EntityBase;
+
+        /// <summary>
+        /// retorna un objeto anidado, retorna el objeto pero un un solo objeto anidado.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expressionConditional"></param>
+        /// <param name="expressionNested"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T> GetWithNestedObject<T>(Expression<Func<T, bool>> expressionConditional,
+            Expression<Func<T, object>> expressionNested, CancellationToken cancellationToken) where T : EntityBase;
+    }
+}
