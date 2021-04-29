faa414830cadcb595f33c5156d37b40ced7296a7
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-04-29 17:31:33 -0500

    creacion del dto y del query para consultar los datos del arduino

---
+++ src/modules/Climate/Cafe.Climate.Application/ArduinoServices/QueryArduinoByRangeTime/ArduinoByRangeTime.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Cafe.Climate.Application.ArduinoServices.QueryArduinoByRangeTime
+{
+    public class ArduinoByRangeTime : IRequest<ArduinoByRangeTimeDTO>
+    {
+        public string CropId { get; set; }
+
+        public DateTime RangeTimelower { get; set; }
+
+        public DateTime RangeTimeUpper { get; set; }
+
+        public List<Claim> Claims { get; set; }
+    }
+}
---
+++ src/modules/Climate/Cafe.Climate.Application/ArduinoServices/QueryArduinoByRangeTime/ArduinoByRangeTimeHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Application.ArduinoServices.QueryArduinoByRangeTime
+{
+    class ArduinoByRangeTimeHandler
+    {
+    }
+}
