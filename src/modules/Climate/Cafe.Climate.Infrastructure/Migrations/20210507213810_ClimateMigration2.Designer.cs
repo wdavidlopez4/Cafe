3c8a05ea2723281f1cb5289c3b340ce3d69d6923
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-07 16:40:01 -0500

    creacion de la migracion de clima v2

--- src/modules/Climate/Cafe.Climate.Infrastructure/Migrations/20210507213810_ClimateMigration2.Designer.cs
+++ src/modules/Climate/Cafe.Climate.Infrastructure/Migrations/20210507213810_ClimateMigration2.Designer.cs
@@ -4,14 +4,16 @@ using Cafe.Climate.Infrastructure.EFcore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cafe.Climate.Infrastructure.Migrations
 {
     [DbContext(typeof(ClimateContext))]
-    partial class ClimateContextModelSnapshot : ModelSnapshot
+    [Migration("20210507213810_ClimateMigration2")]
+    partial class ClimateMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
