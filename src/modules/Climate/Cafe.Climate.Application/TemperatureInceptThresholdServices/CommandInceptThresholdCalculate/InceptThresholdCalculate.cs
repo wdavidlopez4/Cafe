bc1e4472947e84cd3ff2d26563114b86ec88e8bf
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-07 13:46:22 -0500

    estructura de el componente para calcular el umbral optimo de desarrollo

---
+++ src/modules/Climate/Cafe.Climate.Application/TemperatureInceptThresholdServices/CommandInceptThresholdCalculate/InceptThresholdCalculate.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Application.TemperatureInceptThresholdServices.CommandInceptThresholdCalculate
+{
+    public class InceptThresholdCalculate : IRequest<InceptThresholdCalculateDTO>
+    {
+    }
+}
---
+++ src/modules/Climate/Cafe.Climate.Application/TemperatureInceptThresholdServices/CommandInceptThresholdCalculate/InceptThresholdCalculateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Application.TemperatureInceptThresholdServices.CommandInceptThresholdCalculate
+{
+    public class InceptThresholdCalculateDTO
+    {
+    }
+}
