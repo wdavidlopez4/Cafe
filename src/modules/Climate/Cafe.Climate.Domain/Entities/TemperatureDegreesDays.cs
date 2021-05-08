77eda20eb4fdf6e97d215e0b7c96bc06097b03c1
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-08 17:30:48 -0500

    creacion del dominio para los grados dias

---
+++ src/modules/Climate/Cafe.Climate.Domain/Entities/TemperatureDegreesDays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Domain.Entities
+{
+    public class TemperatureDegreesDays : ClimaticFactor
+    {
+        public double DegreesDays { get; private set; } 
+
+        public DateTime Time { get; private set; }
+
+        internal TemperatureDegreesDays(string monitoringId, double DegreesDays, DateTime Time):base(monitoringId)
+        {
+            this.DegreesDays = DegreesDays;
+            this.Time = Time;
+
+            //Falta Validar
+        }
+
+        private TemperatureDegreesDays():base()
+        {
+            //For EF
+        }
+    }
+}
