f5f6de77698011f2974d0d9bf2718ed5e0afe738
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-08 12:41:40 -0500

    creacion del componente para obtener el umbral optimo de desarrollo del incepto por el id del cultivo "esqueleto"

---
+++ src/modules/Climate/Cafe.Climate.Application/TemperatureInceptThresholdServices/QueryInceptThresholdByIdCultive/InceptThresholdByIdCultiveDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe.Climate.Application.TemperatureInceptThresholdServices.QueryInceptThresholdByIdCultive
+{
+    public class InceptThresholdByIdCultiveDTO
+    {
+    }
+}
---
+++ src/modules/Climate/Cafe.Climate.Application/TemperatureInceptThresholdServices/QueryInceptThresholdByIdCultive/InceptThresholdByIdCultiveHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cafe.Climate.Application.TemperatureInceptThresholdServices.QueryInceptThresholdByIdCultive
+{
+    public class InceptThresholdByIdCultiveHandler : IRequestHandler<InceptThresholdByIdCultive, InceptThresholdByIdCultiveDTO>
+    {
+        public Task<InceptThresholdByIdCultiveDTO> Handle(InceptThresholdByIdCultive request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
