05e8eae6e43dc8f0516d647755f25ffe669fbb67
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-08 18:32:34 -0500

    creacion de la migracion para los grados dias migracion climate v4

---
+++ src/modules/Climate/Cafe.Climate.Infrastructure/Migrations/20210508233051_ClimateMigration4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafe.Climate.Infrastructure.Migrations
+{
+    public partial class ClimateMigration4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DegreesDays",
+                table: "ClimaticFactor",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "ClimaticFactor",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DegreesDays",
+                table: "ClimaticFactor");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "ClimaticFactor");
+        }
+    }
+}
